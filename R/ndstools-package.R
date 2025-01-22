86f0295e81e6a3cf64263c328998c4c6bdfb4303
barneyricca <barneyricca@gmail.com>
2025-01-22 13:14:31 -0500

    Add GitHub links to DESCRIPTION

---
+++ R/ndstools-package.R
@@ -0,0 +1,38 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom deSolve ode
+#' @importFrom doremi calculate.fda
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select
+#' @importFrom energy dcor
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 arrow
+#' @importFrom ggplot2 geom_path
+#' @importFrom ggplot2 geom_point
+#' @importFrom ggplot2 geom_segment
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 ggtitle
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 theme_bw
+#' @importFrom ggplot2 unit
+#' @importFrom ggplot2 xlab
+#' @importFrom ggplot2 xlim
+#' @importFrom ggplot2 ylab
+#' @importFrom ggplot2 ylim
+#' @importFrom lme4 lmer
+#' @importFrom magrittr %$%
+#' @importFrom mgcv gam
+#' @importFrom rlang .data
+#' @importFrom stats coef
+#' @importFrom stats complete.cases
+#' @importFrom stats embed
+#' @importFrom stats formula
+#' @importFrom stats lm
+#' @importFrom stats loess
+#' @importFrom stats predict
+#' @importFrom stats quantile
+#' @importFrom tidyr %>%
+## usethis namespace: end
+NULL
